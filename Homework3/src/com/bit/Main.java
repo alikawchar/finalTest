460a6586f1e0808af8f9bd3d6c2d5a248ce006bb
Kawchar Ali <kawcharali@Kawchars-MacBook-Pro.local>
2023-02-17 23:14:00 -0500

    final commit

---
+++ Homework3/src/com/bit/Main.java
@@ -0,0 +1,17 @@
+package com.bit;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		Thursday t = new Thursday();
+		         t.mix1(10, 5.5);
+		         t.mix2("Is this correct?" , 10>5);
+		         t.mix3(100000000, 125, 10);
+		         t.mix4('R');
+		         t.mix5(6);
+		         t.open();
+		         t.find();
+		         t.h(); 
+	}
+}
---
+++ Homework3/src/com/bit/Thursday.java
@@ -0,0 +1,42 @@
+package com.bit;
+
+public class Thursday {
+
+void mix1(int x, double d) {
+		
+		double z=x+d;
+		System.out.println(z);
+		System.out.println(x+z);
+		
+	}
+	 void mix2(String s, boolean b) {
+		System.out.println(s + " "+ b);
+		
+	}
+	 void mix3(long l, int s, int j ) {
+		     long c= l+s; 
+		 System.out.println(c);
+		 
+	 }
+	 void mix4(char x) {
+		 
+		 
+	  System.out.println(x);
+	 }
+	 void mix5(float f ) {
+		 
+		 System.out.println(f);
+		 
+	 }
+	 void open(ChromeDriver j) {
+		 System.out.println(j);
+		 
+	 }
+	 void find(WebElement k) {
+		 System.out.println(k);
+	 }
+	 void h (By o) {
+		System.out.println(o);
+	}
+}
+
